73631cf4cb60eff1b89d325b9830e440384609b2
sunsetzzz <sunsetzzz@gitcode.net>
2024-03-19 21:19:01 +0800

    新增员工功能

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,6 +1,7 @@
 package com.sky.mapper;
 
 import com.sky.entity.Employee;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -12,7 +13,10 @@ public interface EmployeeMapper {
      * @param username
      * @return
      */
-    @Select("select * from employee where username = #{username}")
+    @Select("select * from  sky_take_out.employee where username = #{username}")
     Employee getByUsername(String username);
 
+    //{}里面的数据名对应的是实体类，不是表中的名
+    @Insert("insert into sky_take_out.employee values(null,#{name},#{username},#{password},#{phone},#{sex},#{idNumber},#{status},#{createTime},#{updateTime},#{createUser},#{updateUser})")
+    void save(Employee employee);
 }
